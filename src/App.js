2b993e9f7291b50c9a72928b8896f7870e33a835
CheyReddy <CheyReddy@users.noreply.github.com>
2025-05-25 16:47:13 +0530

    initial commit of markdown previewer app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
+import React from 'react';
+import { marked } from 'marked';
 import './App.css';
 
+marked.setOptions({
+  breaks: true,
+});
 function App() {
+  const [markdown, setMarkdown] = React.useState(`# H1 Heading
+## H2 Subheading
+
+**This is bold text**
+
+Here is a [link](https://www.freecodecamp.org)
+
+Here is some \`inline code\`
+
+\`\`\`js
+// This is a code block
+function greet(name) {
+  return "Hello, " + name;
+}
+\`\`\`
+
+- List item 1
+- List item 2
+- List item 3
+
+> This is a blockquote
+
+![React Logo](https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg)
+`);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="firstSection">
+        <div className="bars">Editor</div>
+        <textarea id="editor" value={markdown}
+        onChange={(e) => setMarkdown(e.target.value)} name="editor" /> 
+      </div>
+      <div className="secondSection">
+        <div className="bars">Previewer</div>
+        <div id="preview" name="preview" dangerouslySetInnerHTML={{ __html: marked.parse(markdown) }}></div> 
+      </div>
     </div>
   );
 }
-
 export default App;
